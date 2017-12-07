d34af7ddc9e4d285574b4e12f65050b60117c38c
Victor Dario Martinez <victor.martinez@globallogic.com>
2017-12-07 14:07:31 -0300

    unifico logica en build.gradle root

--- code/apiGateway/src/main/java/com/directv/afe/api/gateway/ApiGateway.java
+++ code/apiGateway/src/main/java/com/directv/afe/api/gateway/ApiGateway.java
@@ -1,5 +1,6 @@
 package com.directv.afe.api.gateway;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -11,6 +12,6 @@ import org.springframework.stereotype.Controller;
 public class ApiGateway {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ApiGateway.class).web(true).run(args);
+		new SpringApplicationBuilder(ApiGateway.class).web(WebApplicationType.SERVLET).run(args);
 	}
 }
