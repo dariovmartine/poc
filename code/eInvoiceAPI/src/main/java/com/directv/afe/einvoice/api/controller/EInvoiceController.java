72bf3f723542e6f83b5352db7c57df1f6c6f0e0f
Victor Dario Martinez <victor.martinez@globallogic.com>
2017-12-22 15:24:17 -0300

    fix

--- code/eInvoiceAPI/src/main/java/com/directv/afe/einvoice/api/controller/EInvoiceController.java
+++ code/eInvoiceAPI/src/main/java/com/directv/afe/einvoice/api/controller/EInvoiceController.java
@@ -3,6 +3,7 @@ package com.directv.afe.einvoice.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
@@ -22,7 +23,7 @@ public class EInvoiceController {
 	@Autowired
 	EInvoiceGateway eInvoiceGateway;
    
-    @RequestMapping("/invoices")
+    @RequestMapping(value = "/invoices", method= RequestMethod.POST, consumes = "application/json",  produces = "application/json")
     public DeferredResult<EInvoiceResponse> einvoice(@RequestBody(required = true) EInvoiceRequest eInvoiceRequest) {
     	
     	DeferredResult<EInvoiceResponse> deferredResult = new DeferredResult<>();
